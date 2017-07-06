095a06350a26c57ad2026787f2646e3302ddd7cd
kv89 <kalaskarvishald@gmail.com>
2017-07-06 22:38:11 +0530

    learn the sequence of steps that happen when a promise gets initialized and executed !!

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-var Pending = 0;
-var	Fulfilled = 1;
-var	Rejected = 2;
+'use strict';
+
+// var Pending = 0;
+// var	Fulfilled = 1;
+// var	Rejected = 2;
 
 function MyPromise( fn ){// may be executer function !
+	console.log('MyPromise called ..');
 	var callback = null;
 	this.then = function( cb ){
+		console.log('then called ..');
 		callback = cb;
-	}
+	};
 
 	function resolve(value){
-		setTimeout(function(){
+		console.log('MyPromise -> resolve called ..');
+		setTimeout(function(){ // next tick so that callback will get initialized !
+			console.log('MyPromise -> resolve : setTimeout called ..');
 			callback(value);
 		}, 0);
 	}
@@ -19,17 +25,35 @@ function MyPromise( fn ){// may be executer function !
 
 // use promise in your methods !!
 
-// function doSomething(){// define
-// 	return new Promise(function( resolve ){
-// 		setTimeout(function(){
-// 			var fun_output = 42;
-// 			resolve( fun_output );
-// 		}, 3000);
+function doSomething(){// define
+	console.log('doSomething called ..');
+	return new MyPromise(function( resolve ){
+		console.log('MyPromise executor called ..' );
+		var fun_output = 42;
+		resolve( fun_output );
+	});
+}
+
+var pp = doSomething();
+
+// fails as callback is not initialized !!
+// global.setTimeout(function(){
+// 	pp.then(function(value){// call with then
+// 		// console.log('then:listener part called ..');
+// 		console.log(" -> ", value);
+	
 // 	});
-// }
+// }, 0);
 
-// var pp = doSomething();
 
-// pp.then(function(value){// call with then
-// 	console.log(" -> ", value);
-// });
+console.log("calling then noww ...... ");
+console.log("calling then noww ...... ");
+console.log("calling then noww ...... ");
+console.log("calling then noww ...... ");
+pp.then(function(value){// call with then
+	console.log('then:listener part called ..');
+	console.log(" -> ", value);
+});
+
+module.exports = MyPromise;
+console.log("module exported ....");
